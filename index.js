d801a252a641c9dd8e24df8bc598991b6b6d972a
Carol Chen <msfncarol@gmail.com>
2019-04-21 22:39:53 -0400

    created database

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 
-// const express=require('express');
-// var { Client } = require('pg');
+const cool = require('cool-ascii-faces')
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+const {Pool} = require('pg');
 
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl:true
+});
 
 // var client = new Client({database: 'memoryforum'});
 // client.connect();
@@ -28,10 +35,6 @@
 
 //  app.listen(port, () => console.log(`Listening on ${ PORT }`))
 
-const cool = require('cool-ascii-faces')
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
@@ -39,4 +42,16 @@ express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render(res.send ('Hello World!')))
   .get('/cool', (req, res) => res.send(cool()))
+  .get('/db', async (req, res) => {
+      try {
+        const client = await pool.connect()
+        const result = await client.query('SELECT * FROM test_table');
+        const results = { 'results': (result) ? result.rows : null};
+        res.render('/db', results);
+        client.release();
+      } catch (err){
+          console.error(err);
+          res.send("Error" + err);
+      }
+  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
