ce25c973b572afd27e0188723a627ff26c7f09cd
Carol Chen <msfncarol@gmail.com>
2019-04-21 22:02:31 -0400

    Add cool face API

--- index.js
+++ index.js
@@ -1,13 +1,42 @@
-const express=require('express');
+
+// const express=require('express');
+// var { Client } = require('pg');
 
 
-const app = express();
-const port = process.env.PORT || 8000;
+// var client = new Client({database: 'memoryforum'});
+// client.connect();
+
+// const text = 'INTO posts (message) VALUES ($1)';
+// const vlaues = ['I remember a blue rose.']
+
+// // client.query('SELECT * FROM posts', (err, res) => {  //query, vlaue, callback
+// //     if (err) throw err;
+// //     console.log(res);
+// //     client.end();
+// // })
 
 
+// const app = express();
+// const port = process.env.PORT || 8000;
 
-app.get('/', function(req,res){
-    res.send ('Hello World!');
-});
+// console.log(process.env.DATABASE_URL);
 
-app.listen(port, () => console.log('app listening on port 8000!'));
+
+// app.get('/', function(req,res){
+//     res.send ('Hello World!');
+// });
+
+// app.listen(port, () => console.log('app listening on port!'));
+
+const cool = require('cool-ascii-faces')
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('index.js'))
+  .get('/cool', (req, res) => res.send(cool()))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
