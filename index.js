ef5ac461fae47dac40fae8029c2969edb8b52d2b
Carol Chen <msfncarol@gmail.com>
2019-04-21 22:41:09 -0400

    correction

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ express()
   .get('/db', async (req, res) => {
       try {
         const client = await pool.connect()
-        const result = await client.query('SELECT * FROM test_table');
+        const result = await client.query('SELECT * FROM posts');
         const results = { 'results': (result) ? result.rows : null};
         res.render('/db', results);
         client.release();
