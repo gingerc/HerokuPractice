3431560be87e61dcdfbadbb3cbb27e77513a1051
Carol Chen <msfncarol@gmail.com>
2019-04-21 22:03:57 -0400

    correct mistake

--- index.js
+++ index.js
@@ -19,14 +19,14 @@
 // const app = express();
 // const port = process.env.PORT || 8000;
 
-// console.log(process.env.DATABASE_URL);
+// // console.log(process.env.DATABASE_URL);
 
 
 // app.get('/', function(req,res){
 //     res.send ('Hello World!');
 // });
 
-// app.listen(port, () => console.log('app listening on port!'));
+//  app.listen(port, () => console.log(`Listening on ${ PORT }`))
 
 const cool = require('cool-ascii-faces')
 const express = require('express')
@@ -37,6 +37,6 @@ express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('index.js'))
+  .get('/', (req, res) => res.render(res.send ('Hello World!')))
   .get('/cool', (req, res) => res.send(cool()))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
