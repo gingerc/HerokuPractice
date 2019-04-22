e0f29c6fe78ba735750f0d31fc3f20d6415c9060
Carol Chen <msfncarol@gmail.com>
2019-04-22 14:51:59 -0400

    f

--- index.js
+++ index.js
@@ -17,55 +17,54 @@ app.use(bodyParser.urlencoded({    //url support
     extended: true
   })); 
 app.use('/public', express.static('public'));
-const {Client} = require('pg');
+const {Pool} = require('pg');
 
 //HEROKU_POSTGRESQL_MAUVE_URL
 
-// const pool = new Pool({
-//     connectionString: process.env.DATABASE_URL,
-//     ssl:true
-// });
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl:true
+});
 
-let client = new Client({database: 'forum', ssl:true})
-client.connect()
+// let client = new Client({database: 'forum', ssl:true})
+// client.connect()
 
-app.get('/', function (req, res) {
-    client.query('SELECT * FROM forum', (err, resSQL) => {
-        if (err){
-            console.log(err)
-        } else {
-            for (var i = 0; i < resSQL.rows.length; i++) {
-            memories.push("<p>"+ resSQL.rows[i].message  + "</p><br>");
-            res.render('index', {
-                memories
-            })
-          }
-        }
-    })
+// app.get('/', function (req, res) {
+//     client.query('SELECT * FROM forum', (err, resSQL) => {
+//         if (err){
+//             console.log(err)
+//         } else {
+//             for (var i = 0; i < resSQL.rows.length; i++) {
+//             memories.push("<p>"+ resSQL.rows[i].message  + "</p><br>");
+//             res.render('index', {
+//                 memories
+//             })
+//           }
+//         }
+//     })
     
- })
+//  })
 
-//  app.get('/db', async (req, res) => {
-//     try {
-//       const client = await pool.connect()
-//       const result = await client.query('SELECT * FROM forum');
-//       const results = { 'results': (result) ? result.rows : null};
-//       for (var i = 0; i < result.rows.length; i++) {
-//         //log += result.rows[i].message + "<br>";
-//         memories.push("<p>"+ result.rows[i].message  + "</p><br>");
-//         username.push(result.rows[i].username);
-//         message.push(result.rows[i].message);
-//       }
-//       //read the html file first
-//       //document.getElementById("postContainer").innerHTML = memories;
-//       res.send(memories);
-
-//       client.release();
-//     } catch (err){
-//         console.error(err);
-//         res.send("Error" + err);
-//     }
-// })
+ app.get('/db', async (req, res) => {
+    try {
+      const client = await pool.connect()
+      const result = await client.query('SELECT * FROM forum');
+      //const results = { 'results': (result) ? result.rows : null};
+      for (var i = 0; i < result.rows.length; i++) {
+        //log += result.rows[i].message + "<br>";
+        memories.push("<p>"+ result.rows[i].message  + "</p><br>");
+        // username.push(result.rows[i].username);
+        // message.push(result.rows[i].message);
+      }
+      res.render('index', {
+        memories
+    })
+      client.release();
+    } catch (err){
+        console.error(err);
+        res.send("Error" + err);
+    }
+})
 
 app.post('/post', function (req, res){
     var text = req.body.userinput;
