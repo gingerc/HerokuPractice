2c2259931bf63abcfb51e297afb0fabeb8755dc5
Carol Chen <msfncarol@gmail.com>
2019-04-22 00:48:58 -0400

    read forum test

--- index.js
+++ index.js
@@ -1,14 +1,57 @@
-
-const cool = require('cool-ascii-faces')
 const express = require('express')
 const path = require('path')
-const PORT = process.env.PORT || 5000
+const bodyParser = require('body-parser')
+var mustacheExpress = require('mustache-express');
+
+const app = express ();
+
+app.engine('html', mustacheExpress());
+app.set('view engine', 'html');
+app.set('views', __dirname);
+app.use(bodyParser.json())          //json support
+app.use(bodyParser.urlencoded({    //url support
+    extended: true
+  })); 
+
 const {Pool} = require('pg');
 
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
     ssl:true
 });
+
+const PORT = process.env.PORT || 5000
+
+app.use('/public', express.static('public'));
+
+
+app.get('/', function (req, res) {
+    res.sendFile( __dirname + "/" + "index.html" );
+ })
+
+ app.get('/db', async (req, res) => {
+    try {
+      const client = await pool.connect()
+      const result = await client.query('SELECT * FROM posts');
+     // const results = { 'results': (result) ? result.rows : null};
+      res.send(result);
+      client.release();
+    } catch (err){
+        console.error(err);
+        res.send("Error" + err);
+    }
+})
+
+ var postContent = {
+     name : "Carol",
+     message : "Hello"
+ }
+
+//  var postContainer = document.getElementById("postContainer");
+//  var postTemplate = Mustache.render("{{name}} says \"{{message}}\" ", postContent);
+//  postContainer.append(postTemplate);
+
+
 
 
 // var client = new Client({database: 'memoryforum'});
@@ -24,35 +67,13 @@ const pool = new Pool({
 // // })
 
 
-// const app = express();
-// const port = process.env.PORT || 8000;
-
-// // console.log(process.env.DATABASE_URL);
 
 
-// app.get('/', function(req,res){
-//     res.send ('Hello World!');
-// });
-
-//  app.listen(port, () => console.log(`Listening on ${ PORT }`))
+//display the forum 
 
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render(res.send ('Hello World!')))
-  .get('/cool', (req, res) => res.send(cool()))
-  .get('/db', async (req, res) => {
-      try {
-        const client = await pool.connect()
-        const result = await client.query('SELECT * FROM posts');
-        const results = { 'results': (result) ? result.rows : null};
-        console.log(results);
-        client.release();
-      } catch (err){
-          console.error(err);
-          res.send("Error" + err);
-      }
-  })
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+  //app.get('/', (req, res) => res.render(res.send ('Hello World!')))
+  app.get('/cool', (req, res) => res.send(cool()))
+  
+  app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
